f4d0489d9ab2d848b176c9624ab5d96ada283687
giancarlomicelifacul <giancarlo.miceli@aluno.faculdadeimpacta.com.br>
2025-10-29 22:23:33 -0300

    feat: error view

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cambio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
